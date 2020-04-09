2fc60f635d4830b88fba8f337dbf528b09ade9e0
FerdiantJoshua <ferdiant.98@gmail.com>
2020-04-09 14:10:36 +0700

    Combine view and edit user_details to one page

--- account/models.py
+++ account/models.py
@@ -17,9 +17,9 @@ class UserDetails(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
     gender = models.CharField(
         max_length=1,
-        choices=GENDER_CHOICES,
+        choices=GENDER_CHOICES
     )
-    date_of_birth = models.DateField()
+    date_of_birth = models.DateField(null=True)
     address = models.CharField(max_length=96)
     phone_validator = RegexValidator(regex=r'0[\d]{9,12}', message='Phone number must start with 0. Up to 13 digits allowed.')
     phone_number = models.CharField(validators=[phone_validator], max_length=13)
@@ -29,4 +29,4 @@ class UserDetails(models.Model):
     experiences_proofs = models.ImageField(upload_to='account/proof/%Y%m%d', blank=True)
 
     def __str__(self):
-        return 'User with id ' + str(self.user)
+        return 'Userdetails with user ' + str(self.user)
--- account/views.py
+++ account/views.py
@@ -1,9 +1,11 @@
-from django.contrib.auth import login, authenticate
+from django.contrib.auth import login
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.views import LoginView
 from django.http import HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse_lazy
 from django.views import generic
+from django.views.generic.detail import SingleObjectMixin
 
 from .forms import RegistrationForm, LoginForm, UserDetailsForm
 from .models import User, UserDetails
@@ -24,6 +26,7 @@ class EnhancedLoginView(LoginView):
     model = User
     form_class = LoginForm
     success_url = 'account:user_details'
+    redirect_authenticated_user = True
 
     def __init__(self, *args, **kwargs):
         super(EnhancedLoginView, self).__init__(*args, **kwargs)
@@ -37,12 +40,22 @@ class RegisterView(generic.FormView):
     model = User
     template_name = 'registration/register.html'
     form_class = RegistrationForm
-    success_url = 'account:user_details'
+    success_url = reverse_lazy('account:user_details')
+
+    def dispatch(self, request, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            redirect_to = self.get_success_url()
+            if redirect_to == self.request.path:
+                raise ValueError(
+                    "Redirection loop for authenticated user detected. Check that "
+                    "your LOGIN_REDIRECT_URL doesn't point to a login page."
+                )
+            return HttpResponseRedirect(redirect_to)
+        return super().dispatch(request, *args, **kwargs)
 
     def form_valid(self, form):
-        data = form.cleaned_data
-        form.save(data)
-        user = authenticate(self.request, username=data['username'], password1=data['password1'])
+        user = form.save()
+        UserDetails(user=user).save()
         login(self.request, user)
         return super().form_valid(form)
 
@@ -50,20 +63,24 @@ class RegisterView(generic.FormView):
     def form_invalid(self, form):
         return super().form_invalid(form)
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        print(context)
-        return context
-
 
 class UserDetailsView(LoginRequiredMixin, generic.FormView):
-    model = UserDetails
+    # model = UserDetails
     template_name = 'account/user_details.html'
     form_class = UserDetailsForm
-    success_url = '/'
-
+    success_url = reverse_lazy('account:user_details')
     login_url = 'account:login'
     # redirect_field_name = 'redirect_to'
+
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
+        try:
+            user_details = UserDetails.objects.get(pk=self.request.user)
+        except:
+            user_details = UserDetails(user=self.request.user)
+            user_details.save()
+        kwargs.update({'instance': user_details})
+        return kwargs
 
     def get_form(self, form_class=None):
         form = super().get_form(form_class)
@@ -77,11 +94,17 @@ class UserDetailsView(LoginRequiredMixin, generic.FormView):
             form.fields['experiences_proofs'].widget.attrs.update({'required':''})
         return form
 
-    @add_invalid_css_class_to_form
-    def form_invalid(self, form):
-        return super().form_invalid(form)
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        user = self.request.user
+        context['user'] = user
+        return context
 
     def form_valid(self, form):
         form.instance.user = self.request.user
         form.save()
         return super().form_valid(form)
+
+    @add_invalid_css_class_to_form
+    def form_invalid(self, form):
+        return super().form_invalid(form)
