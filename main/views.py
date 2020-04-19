2fe7aabafa6a99fb9231b7a880b3bce4de13eadd
FerdiantJoshua <ferdiant.98@gmail.com>
2020-04-19 22:53:01 +0700

    Update views.py

--- main/views.py
+++ main/views.py
@@ -1,9 +1,4 @@
-from account.models import User
-from django.shortcuts import render
 from django.views import generic
-from belajar_online import settings
-from django.template import RequestContext
-from django.http import HttpResponse
 
 
 class IndexView(generic.TemplateView):
